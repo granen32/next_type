a99514322182eb382f303c6bda86be9ace99ebbc
granen32 <granen32@eigene.ai>
2023-06-22 18:32:39 +0900

    [FEAT] SEO 파일 생성

---
+++ app/list/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const page = () => {
+  return (
+    <>
+      <div>
+        <h4>상품목록</h4>
+        <div>
+          <h4>상품 1 $40</h4>
+        </div>
+        <div>
+          <h4>상품 2 $30</h4>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default page;
